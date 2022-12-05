2439b0f402b89fe558be676fc04582e0d84cd911
Brian Pontes <brian.robert16@hotmail.com>
2022-12-05 13:27:34 -0300

    estilizando app

--- first-module/src/App.jsx
+++ first-module/src/App.jsx
@@ -1,5 +1,18 @@
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+import './global.css'
+import styles from './App.module.css'
+
 function App() {
-  return <h1>Hello ignite</h1>
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>Teste</main>
+      </div>
+    </>
+  )
 }
 
 export default App
